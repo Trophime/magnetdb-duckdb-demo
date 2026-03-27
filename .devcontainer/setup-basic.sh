0526a514caa8906edf48c044738318821fdb09cb
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2026-03-27 15:16:19 +0100

    add submodel install

--- .devcontainer/setup-basic.sh
+++ .devcontainer/setup-basic.sh
@@ -5,6 +5,7 @@ echo "🚀 Starting Isolated Environment Setup..."
 
 # 1. Create a clean venv (no system packages)
 python3 -m venv .venv --system-site-packages
+# shellcheck source=/dev/null
 source .venv/bin/activate
 
 # 2. Upgrade pip to avoid old-version headaches
@@ -18,6 +19,9 @@ pip install ipyfilechooser ipywidgets voila pandas
 # 5. Install your 'Heavy Development' submodules in EDITABLE mode
 # echo "🛠️ Linking development submodules..."
 # pip install -e ./submodules/my-interactive-pkg
+pip install -e "./python_magnetgeo"
+pip install -e "./python_magnetcooling[fitting]"
+#pip install -e "./python_magnetrun[signal]"
 
 echo "✅ Setup Complete! Your environment is isolated but has access to system site packages."
 
